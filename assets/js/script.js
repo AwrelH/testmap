4c6daf1a4a8443c51cf26dd7b4e25368e436259f
AwrelH <hiwa.awrel@gmail.com>
2022-08-03 16:40:32 +0000

    add eventlistener to regions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,70 @@
-
-//document.addEventListener('click', myRegion);
-
-const regions = { alternatives : ['abruzzo', 'aosta', 'basilicata', 'calabria', 'campania',
-                    'emilia-romagna', 'friuli-venezia-giulia', 'lazio',
-                    'liguria', 'lombardia', 'marche','molise', 'piemonte',
-                    'puglia', 'sardegna', 'sicilia', 'toscana', ' trentino',
-                    'umbria', 'veneto' ]
+/*
+****the regions****
+*/
+const regions = {
+    alternatives: ['abruzzo', 'aosta', 'basilicata', 'calabria', 'campania',
+        'emilia-romagna', 'friuli-venezia-giulia', 'lazio',
+        'liguria', 'lombardia', 'marche', 'molise', 'piemonte',
+        'puglia', 'sardegna', 'sicilia', 'toscana', ' trentino',
+        'umbria', 'veneto']
 }
 
+/*
+****getting document information class of bild****
+*/
+//let regionName = document.querySelectorAll('.bild');
+//ADD questions to questionrow
+document.getElementById('questionRow').innerHTML = `Where is the following region<br> ${regions.alternatives[i]}`;
 
-let regionName = document.querySelectorAll('.bild');
-document.getElementById('questionRow').innerHTML = `Click on ${regions.alternatives[1]}`;
-
-
-
+/*
+****for loop for my regions****
+*/
 let arr = regions.alternatives
 function questions() {
     for (let i = 0; i < arr.length; i++) {
         console.log(arr[i])
-        
+
     }
 }
+
+
+
+/*
+****keep score****
+****starting from 0
+*/
 correct = 0;
 incorrect = 0;
+ind = 0;
+
 questions()
+
+/*
+****Start function****
+*/
+function startGame() { }
+
+/**
+ * Eventlisteners to regions, alertmessage with data-title
+ */
+
+let elements = document.querySelectorAll('.bild');
+elements.forEach(funk => funk.addEventListener('click', event => {
+   console.log(event.target.getAttribute("data-title"));
+   alert(event.target.getAttribute('data-title'))
+  }));
+
+//let funk = function() {
+//    let attribute = this.getAttribute("data-title");
+//    alert(attribute);}
+    
+
+
+
+
+//for (let i = 0; i < elements.length; i++) {
+//    elements[i].addEventListener('click', funk, false)
+    
+       
+    
+//}
