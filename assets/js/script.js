80ae70926cf86efbd89018147a6f508a74fb8c87
AwrelH <hiwa.awrel@gmail.com>
2022-08-03 12:34:48 +0000

    little js started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 
-document.addEventListener('click', myRegion);
+//document.addEventListener('click', myRegion);
 
-function myRegion() {
-    document.getElementsByClassName('.bild')
-    console.log('this is a region')
+const regions = { alternatives : ['abruzzo', 'aosta', 'basilicata', 'calabria', 'campania',
+                    'emilia-romagna', 'friuli-venezia-giulia', 'lazio',
+                    'liguria', 'lombardia', 'marche','molise', 'piemonte',
+                    'puglia', 'sardegna', 'sicilia', 'toscana', ' trentino',
+                    'umbria', 'veneto' ]
 }
+
+
+let regionName = document.querySelectorAll('.bild');
+document.getElementById('questionRow').innerHTML = `Click on ${regions.alternatives[1]}`;
+
+
+
+let arr = regions.alternatives
+function questions() {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+        
+    }
+}
+correct = 0;
+incorrect = 0;
+questions()
