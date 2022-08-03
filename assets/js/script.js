b787ec3c234e553195c18d1371f88e5cf5da74c0
AwrelH <hiwa.awrel@gmail.com>
2022-08-03 19:37:06 +0000

    if statement works, but needs more

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,9 @@ const regions = {
 */
 //let regionName = document.querySelectorAll('.bild');
 //ADD questions to questionrow
-document.getElementById('questionRow').innerHTML = `Where is the following region<br> ${regions.alternatives[i]}`;
+
+
+
 
 /*
 ****for loop for my regions****
@@ -33,9 +35,7 @@ function questions() {
 ****keep score****
 ****starting from 0
 */
-correct = 0;
-incorrect = 0;
-ind = 0;
+
 
 questions()
 
@@ -44,14 +44,38 @@ questions()
 */
 function startGame() { }
 
+let i = 0;
+ let qBar = document.getElementById('questionRow');
+ qBar.innerHTML = `Where is the following region<br> ${regions.alternatives[i]}`;
 /**
  * Eventlisteners to regions, alertmessage with data-title
  */
+i = 0;
+let wrong = document.getElementById('wrong');
+let right = document.getElementById('right');
+wrong.innerHTML = `Incorrect:${i}`
+right.innerHTML = `Correct:${i}`
 
+
+// du ska se till att det kommer ny fråga + att poäng fördelas efter svar. Även ny fråga vid fel svar? 
 let elements = document.querySelectorAll('.bild');
 elements.forEach(funk => funk.addEventListener('click', event => {
    console.log(event.target.getAttribute("data-title"));
-   alert(event.target.getAttribute('data-title'))
+   if (event.target.getAttribute("data-title") === regions.alternatives[i] ) {
+    document.body.style.backgroundColor = 'yellow';
+    right += i;
+    regions.alternatives[i]++;
+    
+    
+    
+
+
+}else {
+    wrong += i;
+}
+   //alert(event.target.getAttribute('data-title'));
+   
+
   }));
 
 //let funk = function() {
@@ -63,8 +87,4 @@ elements.forEach(funk => funk.addEventListener('click', event => {
 
 
 //for (let i = 0; i < elements.length; i++) {
-//    elements[i].addEventListener('click', funk, false)
-    
-       
-    
-//}
+//    elements[i].addEventListener('click', funk, false)}
