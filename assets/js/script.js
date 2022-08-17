f9afd00c8dacd2dbf2fc13654b84955c8cdae83d
AwrelH <hiwa.awrel@gmail.com>
2022-08-17 08:50:05 +0000

    random region displayed now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 /*
 ****the regions****
 */
@@ -23,13 +25,20 @@ const regions = {
 */
 let arr = regions.alternatives
 function questions() {
+  
     for (let i = 0; i < arr.length; i++) {
         console.log(arr[i])
+        
 
     }
+
 }
 
-
+/**
+ * random region
+ */
+let regionRandom = Math.floor(Math.random()* arr.length)
+let nameTheRegion = regions.alternatives[regionRandom]
 
 /*
 ****keep score****
@@ -46,11 +55,13 @@ function startGame() { }
 
 let i = 0;
  let qBar = document.getElementById('questionRow');
- qBar.innerHTML = `Where is the following region<br> ${regions.alternatives[i]}`;
+ qBar.innerHTML = `Where is the following region<br> ${nameTheRegion}`;
 /**
  * Eventlisteners to regions, alertmessage with data-title
  */
 i = 0;
+rightCount=0;
+wrongCount=0;
 let wrong = document.getElementById('wrong');
 let right = document.getElementById('right');
 wrong.innerHTML = `Incorrect:${i}`
@@ -59,24 +70,30 @@ right.innerHTML = `Correct:${i}`
 
 // du ska se till att det kommer ny fråga + att poäng fördelas efter svar. Även ny fråga vid fel svar? 
 let elements = document.querySelectorAll('.bild');
-elements.forEach(funk => funk.addEventListener('click', event => {
-   console.log(event.target.getAttribute("data-title"));
-   if (event.target.getAttribute("data-title") === regions.alternatives[i] ) {
-    document.body.style.backgroundColor = 'yellow';
-    right += i;
-    regions.alternatives[i]++;
+
+elements.forEach(region => {
+    region.addEventListener('click', (e) => {
+        console.log(e.target.getAttribute('data-title'))
+    
+    } )
+    
+});
+
+
+
+// elements.forEach(funk => funk.addEventListener('click', event => {
+//    console.log(event.target.getAttribute("data-title"));
+ //   if (e.target.getAttribute("data-title") === regions.alternatives[i] ) {
+   //  document.body.style.backgroundColor = 'yellow';
+   //  right += i;
+   //  regions.alternatives[i]++;
+//     debugger;
     
     
     
 
 
-}else {
-    wrong += i;
-}
-   //alert(event.target.getAttribute('data-title'));
-   
-
-  }));
+ 
 
 //let funk = function() {
 //    let attribute = this.getAttribute("data-title");
